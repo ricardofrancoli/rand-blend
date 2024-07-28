60d4c009ca4bd921392f1ec53ef89f9540fa94d6
Ricardo Francoli <francoliricardo@gmail.com>
2024-07-29 00:37:20 +0200

    fix(api): use only the envs that are actually used

--- apps/api/src/config.ts
+++ apps/api/src/config.ts
@@ -2,14 +2,12 @@ import z from 'zod'
 
 const config = z
   .object({
-    DEV_APP_BASE_URL: z.string(),
     CLIENT_ID: z.string(),
-    PROD_APP_BASE_URL: z.string(),
-    REDIRECT_URI: z.string()
+    PROD_APP_BASE_URL: z.string()
   })
   .parse(process.env)
 
 const isProd = process.env.NODE_ENV === 'production'
 
-export const APP_BASE_URL = isProd ? config.PROD_APP_BASE_URL : config.DEV_APP_BASE_URL
-export const { CLIENT_ID, REDIRECT_URI } = config
+export const APP_BASE_URL = isProd ? config.PROD_APP_BASE_URL : 'http://localhost:5173'
+export const { CLIENT_ID } = config
