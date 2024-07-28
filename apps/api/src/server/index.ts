5e3a13b36574d98fe9f5ffef4968b50250cf5d35
Ricardo Francoli <francoliricardo@gmail.com>
2024-07-28 23:14:32 +0200

    feat(api|chore): add host for render

--- apps/api/src/server/index.ts
+++ apps/api/src/server/index.ts
@@ -1,7 +1,8 @@
 import { app } from '../routes'
+const host = 'RENDER' in process.env ? `0.0.0.0` : `localhost`
 
 try {
-  await app.listen({ port: 3003 })
+  await app.listen({ port: 3003, host })
   console.log(`Listening on port ${3003}`)
 } catch (err) {
   app.log.error(err)
